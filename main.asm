1f38c12bf5eca75698a90784bfd078c1b6794b2c
bakaraw <142462547+bakaraw@users.noreply.github.com>
2025-02-22 18:43:52 +0800

    added output_subjects.inc
    - this is responsible for outputing subjects

--- main.asm
+++ main.asm
@@ -10,7 +10,7 @@ INCLUDELIB \masm32\lib\kernel32.lib
 INCLUDELIB \masm32\lib\masm32.lib
 
 ; imports the procedures from txt_reader.inc
-INCLUDE includes\txt_reader.inc
+INCLUDE includes\output_subjects.inc
 
 .DATA
     prompt           db "Enter your year in college (1-4): ", 0
@@ -20,14 +20,6 @@ INCLUDE includes\txt_reader.inc
     sem3txt          db 9, 9, "[3] Summer", 10, 10, 0
     yearMsg          db "You entered: ", 0
 
-    first_year_sem1  db "prospectus\\first_year_sem1.txt", 0
-    first_year_sem2  db "prospectus\\first_year_sem2.txt", 0
-    second_year_sem1 db "prospectus\\second_year_sem1.txt", 0
-    second_year_sem2 db "prospectus\\second_year_sem2.txt", 0
-    third_year_sem1  db "prospectus\\third_year_sem1.txt", 0
-    third_year_sem2  db "prospectus\\third_year_sem2.txt", 0
-    fourth_year_sem1 db "prospectus\\fourth_year_sem1.txt", 0
-    fourth_year_sem2 db "prospectus\\fourth_year_sem2.txt", 0
 
 .DATA?
     yearInput        db 4 dup(?)
@@ -43,7 +35,7 @@ main PROC
                invoke StdIn, ADDR yearInput, SIZEOF yearInput
                invoke atodw, ADDR yearInput
                mov    yearNum, eax
-              invoke dwtoa, yearNum, ADDR yearInput
+               invoke dwtoa, yearNum, ADDR yearInput
 .IF yearNum < 1 || yearNum > 4
                jmp    year_input
 .ENDIF
@@ -63,27 +55,8 @@ main PROC
 .IF semNum < 1 || semNum > 3
               jmp    sem_input
 .ENDIF
-              invoke dwtoa, semNum, ADDR semInput
-
-    
-.IF yearNum == 1 && semNum == 1
-          invoke ReadFileProc, ADDR first_year_sem1
-.ELSEIF yearNum == 1 && semNum == 2
-          invoke ReadFileProc, ADDR first_year_sem2
-.ELSEIF yearNum == 2 && semNum == 1
-          invoke ReadFileProc, ADDR second_year_sem1
-.ELSEIF yearNum == 2 && semNum == 2
-          invoke ReadFileProc, ADDR second_year_sem2
-.ELSEIF yearNum == 3 && semNum == 1
-          invoke ReadFileProc, ADDR third_year_sem1
-.ELSEIF yearNum == 3 && semNum == 2
-          invoke ReadFileProc, ADDR third_year_sem2
-.ELSEIF yearNum == 4 && semNum == 1
-          invoke ReadFileProc, ADDR fourth_year_sem1
-.ELSEIF yearNum == 4 && semNum == 2
-          invoke ReadFileProc, ADDR fourth_year_sem2
-.ENDIF
-
+          invoke dwtoa, semNum, ADDR semInput
+          invoke ShowSubjects, yearNum, semNum
           invoke ExitProcess, 0
 main ENDP
-END main
+END main  ; Specify the entry point here
