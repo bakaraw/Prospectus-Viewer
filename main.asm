7b86e6bfc0a94a029d11f9f29c63cb3a90e4737c
bakaraw <142462547+bakaraw@users.noreply.github.com>
2025-02-22 18:53:05 +0800

    Slight refactor sa main
    - README file updated

--- main.asm
+++ main.asm
@@ -9,33 +9,33 @@ INCLUDE \masm32\include\masm32.inc
 INCLUDELIB \masm32\lib\kernel32.lib
 INCLUDELIB \masm32\lib\masm32.lib
 
-; imports the procedures from txt_reader.inc
+; imports the procedures from output_subjects.inc
 INCLUDE includes\output_subjects.inc
 
 .DATA
-    prompt           db "Enter your year in college (1-4): ", 0
-    prompt2          db "Enter semester: ", 10, 0
-    sem1txt          db 9, 9, "[1] 1st Semester", 10, 0
-    sem2txt          db 9, 9, "[2] 2nd Semester", 10, 0
-    sem3txt          db 9, 9, "[3] Summer", 10, 10, 0
-    yearMsg          db "You entered: ", 0
+    prompt      db "Enter your year in college (1-4): ", 0
+    prompt2     db "Enter semester: ", 10, 0
+    sem1txt     db 9, 9, "[1] 1st Semester", 10, 0
+    sem2txt     db 9, 9, "[2] 2nd Semester", 10, 0
+    sem3txt     db 9, 9, "[3] Summer", 10, 10, 0
+    yearMsg     db "You entered: ", 0
 
 
 .DATA?
-    yearInput        db 4 dup(?)
-    semInput         db 4 dup(?)
-    yearNum          dd ?
-    semNum           dd ?
+    inputBuffer db 4 dup(?)
+    yearNum     dd ?
+    semNum      dd ?
 .CODE
 main PROC
     year_input:
                invoke ClearScreen
-               invoke StdOut, ADDR newline
                invoke StdOut, ADDR prompt
-               invoke StdIn, ADDR yearInput, SIZEOF yearInput
-               invoke atodw, ADDR yearInput
+               invoke StdIn, ADDR inputBuffer, SIZEOF inputBuffer
+    ; converts the input string to a double word
+               invoke atodw, ADDR inputBuffer
                mov    yearNum, eax
-               invoke dwtoa, yearNum, ADDR yearInput
+               invoke dwtoa, yearNum, ADDR inputBuffer
+    ; loops when the user enters a value that is not in the range of 1-4
 .IF yearNum < 1 || yearNum > 4
                jmp    year_input
 .ENDIF
@@ -48,14 +48,15 @@ main PROC
               invoke StdOut, ADDR sem1txt
               invoke StdOut, ADDR sem2txt
               invoke StdOut, ADDR sem3txt
-              invoke StdIn, ADDR semInput, SIZEOF semInput
-              invoke atodw, ADDR semInput
+              invoke StdIn, ADDR inputBuffer, SIZEOF inputBuffer
+              invoke atodw, ADDR inputBuffer
               mov    semNum, eax
-              invoke dwtoa, semNum, ADDR semInput
+              invoke dwtoa, semNum, ADDR inputBuffer
+    ; loops when the user enters a value that is not in the range of 1-3
 .IF semNum < 1 || semNum > 3
               jmp    sem_input
 .ENDIF
-          invoke dwtoa, semNum, ADDR semInput
+          invoke dwtoa, semNum, ADDR inputBuffer
           invoke ShowSubjects, yearNum, semNum
           invoke ExitProcess, 0
 main ENDP
