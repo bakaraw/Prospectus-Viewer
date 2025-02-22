3562d4a9f09cd7d1c91368fbd16db3e5ec319189
bakaraw <142462547+bakaraw@users.noreply.github.com>
2025-02-22 09:44:43 +0800

    Revert "added input for year"
    This reverts commit 5ec8ff69007650ff803427a579debf8988b0012d.

--- main.asm
+++ main.asm
@@ -10,18 +10,10 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
-    prompt db "Enter your year of college (1-4): ", 0
-    inputBuffer db 4 dup(0)
-    yearMsg db "You entered: ", 0
-    newline db 13, 10, 0
+    hello db "Hello, World!", 0
 
 .code 
 start:
-    invoke StdOut, addr newline
-    invoke StdOut, addr prompt
-    invoke StdIn, addr inputBuffer, 4
-    invoke StdOut, addr yearMsg
-    invoke StdOut, addr inputBuffer
-    invoke StdOut, addr newline
+    invoke StdOut, addr hello
     invoke ExitProcess, 0
 end start
